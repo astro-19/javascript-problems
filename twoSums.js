da34ecf20760eeaa25c6ce3f86c5ae7976fd2c31
pushkar <pushkar@happlabs.in>
2024-02-17 22:24:43 +0530

    new changes in twoSums

--- twoSums.js
+++ twoSums.js
@@ -1,28 +1,27 @@
-let a = [1, 2, 3, 4, 5, 6, 7]
+let a = [1, 2, 3, 4, 5, 6, 7];
 
 // function twoSum(nums, target) {
-//     let index = []
-//     for (let i = 0; i < nums.length; i++) {
-//         for (let j = i + 1; j < nums.length; j++) {
-//             if (nums[i] + nums[j] === target) {
-//                 index.push([nums[i], nums[j]])
-//             }
-//         }
+//   let index = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         index.push([nums[i], nums[j]]);
+//       }
 //     }
-//     return index
+//   }
+//   return index;
 // }
 
 function twoSum(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j]
-            }
-        }
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [nums[i], nums[j]];
+      }
     }
-};
+  }
+}
 
-
-console.time()
-console.log(twoSum(a, 9))
-console.timeEnd()
+console.time();
+console.log(twoSum(a, 9));
+console.timeEnd();
