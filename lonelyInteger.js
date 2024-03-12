a16108f7e74313c4d7ef6c725883608cbd4df2c6
Pushkar Pandey <pushkartas2418@gmail.com>
2024-03-12 15:20:24 +0530

    new practice code

--- lonelyInteger.js
+++ lonelyInteger.js
@@ -1,24 +1,39 @@
-function lonelyValue(a) {
-  if (a.length === 1) {
-    return a[0];
-  }
+// function lonelyValue(a) {
+//   if (a.length === 1) {
+//     return a[0];
+//   }
 
-  let counts = {};
+//   let counts = {};
 
-  for (let i = 0; i < a.length; i++) {
-    if (!counts[a[i]]) {
-      counts[a[i]] = 1;
-    } else {
-      counts[a[i]] += 1;
-    }
-  }
-  let unique = {};
-  for (let key in counts) {
-    if (counts[key] === 1) {
-      unique[key] = counts[key];
-    }
-  }
-  return unique;
+//   for (let i = 0; i < a.length; i++) {
+//     if (!counts[a[i]]) {
+//       counts[a[i]] = 1;
+//     } else {
+//       counts[a[i]] += 1;
+//     }
+//   }
+//   let unique = {};
+//   for (let key in counts) {
+//     if (counts[key] === 1) {
+//       unique[key] = counts[key];
+//     }
+//   }
+//   return unique;
+// }
+
+
+// let a = [1, 2, 3, 4, 3, 2, 1]
+// let num = []
+// for (let i = 0; i < a.length; i++) {
+//   if (a.indexOf(a[i]) === a.lastIndexOf(a[i])) {
+//     num.push(a[i]);
+//   }
+// }
+
+let num = []
+for (var i = 0; i < 5; i++); {
+  num.push(i + 1)
 }
 
-console.log(lonelyValue([1, 2, 3, 4, 3, 2, 1]));
+console.log(num);
+// console.log(lonelyValue([1, 2, 3, 4, 3, 2, 1]));
---
+++ palindrome.js
@@ -0,0 +1,15 @@
+const test = (text) => {
+    if (typeof text !== 'string') {
+        return 'String should not be empty!';
+    }
+    if (text.length <= 1) {
+        return true;
+    }
+    if (text[0] !== text[text.length - 1]) {
+        return false;
+    } else {
+        return test(text.slice(1, text.length - 1));
+    }
+};
+
+console.log(test("aalllaa"))
