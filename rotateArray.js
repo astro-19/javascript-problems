4db2bc5474c54c59ee9549364676c98cf8a99cf2
Pushkar Pandey <pushkartas2418@gmail.com>
2024-03-15 21:45:54 +0530

    new practice code

--- rotateArray.js
+++ rotateArray.js
@@ -1,5 +1,6 @@
 
 
 const arr = [1, 3, 6, 5, 4];
-const rotatedArr = arr.map((e, i) => arr[arr.length - i - 1])
+let or = 1
+const rotatedArr = arr.map((e, i) => arr[arr.length - i - or])
 console.log(rotatedArr)
