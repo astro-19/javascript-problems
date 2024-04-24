97bdc8f5d6f658bda21ba1045052e8a8bce485e4
Pushkar Pandey <pushkartas2418@gmail.com>
2024-04-24 13:38:06 +0530

    new practice code

---
+++ removeSpecifiEleFromArray.js
@@ -0,0 +1,12 @@
+// Function to remove an element from an array
+function remove_array_element(array, n) {
+    var index = array.indexOf(n);
+
+    if (index > -1) {
+        array.splice(index, 1);
+    }
+
+    return array;
+}
+
+console.log(remove_array_element([2, 5, 9, 6], 5));
