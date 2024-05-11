350f9cef7453b162b5bee1ed01c3bf95ab1b9e22
Pushkar Pandey <pushkartas2418@gmail.com>
2024-05-11 19:23:54 +0530

    new practice code

---
+++ generateArrFromSpecificEntry.js
@@ -0,0 +1,15 @@
+// Function to generate an array of numbers between two values (inclusive)
+function rangeBetween(start, end) 
+{
+  if (start > end) {
+    const arr = Array.from({ length: start - end + 1 }, (_, index) => start - index);
+    return arr;
+  } else {
+    const arr = Array.from({ length: end - start + 1 }, (_, index) => start + index);
+    return arr;
+  }
+}
+
+console.log(rangeBetween(4, 7));
+
+console.log(rangeBetween(-4, 7));
