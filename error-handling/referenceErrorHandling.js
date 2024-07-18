6c267f03e0893ecc61e4e47d3389368ae883cf5d
Pushkar Pandey <pushkartas2418@gmail.com>
2024-07-18 21:47:36 +0530

    new practice code

---
+++ error-handling/referenceErrorHandling.js
@@ -0,0 +1,13 @@
+function access_Variable() {
+    try {
+      console.log(undefinedVariable);
+    } catch (error) {
+      if (error instanceof ReferenceError) {
+        console.log('ReferenceError:', error.message);
+      } else {
+        console.log('Error:', error.message);
+      }
+    }
+  }
+  access_Variable();
+  
