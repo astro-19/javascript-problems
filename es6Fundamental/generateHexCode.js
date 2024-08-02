5d287d41ed181c5b2ad2d594dce2a72346e66da7
Pushkar Pandey <pushkartas2418@gmail.com>
2024-08-02 22:38:40 +0530

    new practice code

---
+++ es6Fundamental/generateHexCode.js
@@ -0,0 +1,7 @@
+// Define a function called `random_hex_color_code` that generates a random hexadecimal color code.
+const random_hex_color_code = () => {
+    let n = (Math.random() * 0xfffff * 1000000).toString(16);
+    return '#' + n.slice(0, 6);
+  };
+  console.log(random_hex_color_code());
+  
