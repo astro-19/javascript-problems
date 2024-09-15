ec2fa30ee044d881ecda99d90aab97dfe2df60d3
Pushkar Pandey <pushkartas2418@gmail.com>
2024-09-15 21:04:44 +0530

    new practice code

---
+++ es6Fundamental/getURLParameter.js
@@ -0,0 +1,12 @@
+// Define a function 'get_URL_Parameters' to extract URL parameters from a given URL.
+const get_URL_Parameters = (url) =>
+  (url.match(/([^?=&]+)(=([^&]*))/g) || []).reduce(
+    (a, v) => (
+      (a[v.slice(0, v.indexOf("="))] = v.slice(v.indexOf("=") + 1)), a
+    ),
+    {}
+  );
+
+console.log(get_URL_Parameters("http://url.com/page?name=Adam&surname=Smith"));
+console.log(get_URL_Parameters("google.com"));
+console.log(get_URL_Parameters("https://www.w3resource.com"));
