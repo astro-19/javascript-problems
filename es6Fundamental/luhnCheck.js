baca73f4c0b1247fe32175b912931a54dc2099b8
Pushkar Pandey <pushkartas2418@gmail.com>
2024-10-16 23:02:21 +0530

    new practice code

---
+++ es6Fundamental/luhnCheck.js
@@ -0,0 +1,18 @@
+// Define the function 'luhnCheck' to perform the Luhn algorithm check on a given number.
+const luhnCheck = (num) => {
+  let arr = (num + "")
+    .split("")
+    .reverse()
+    .map((x) => parseInt(x));
+  let lastDigit = arr.splice(0, 1)[0];
+  let sum = arr.reduce(
+    (acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9),
+    0
+  );
+  sum += lastDigit;
+  return sum % 10 === 0;
+};
+
+console.log(luhnCheck("4485275742308327"));
+console.log(luhnCheck(6011329933655299));
+console.log(luhnCheck(123456789));
