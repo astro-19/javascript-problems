8fb39edd9029e5c694a1ecda2bfcd19ce8c3d28a
Pushkar Pandey <pushkartas2418@gmail.com>
2024-10-21 19:36:55 +0530

    new practice code

---
+++ es6Fundamental/mask.js
@@ -0,0 +1,7 @@
+// Define the function 'mask' to mask part of a credit card number.
+const mask = (cc, num = 4, mask = "*") =>
+  ("" + cc).slice(0, -num).replace(/./g, mask) + ("" + cc).slice(-num);
+
+console.log(mask(1234567890));
+console.log(mask(1234567890, 3));
+console.log(mask(1234567890, -4, "$"));
