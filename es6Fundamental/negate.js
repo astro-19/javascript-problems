889498a94abe1c048f85724ed610afc8094efdbb
Pushkar Pandey <pushkartas2418@gmail.com>
2024-10-26 20:57:13 +0530

    new practice code

---
+++ es6Fundamental/negate.js
@@ -0,0 +1,7 @@
+// Define the function 'negate' to negate the result of a given function.
+const negate =
+  (func) =>
+  (...args) =>
+    !func(...args);
+
+console.log([1, 2, 3, 4, 5, 6].filter(negate((n) => n % 2 === 0)));
