6c711c071598492ed3d6c10e10aaf1000ed3385c
Pushkar Pandey <pushkartas2418@gmail.com>
2024-10-27 21:29:09 +0530

    new practice code

---
+++ es6Fundamental/nest.js
@@ -0,0 +1,15 @@
+// Define the 'nest' function to create a nested structure from flat data based on parent-child relationships.
+const nest = (items, id = null, link = "parent_id") =>
+  items
+    .filter((item) => item[link] === id)
+    .map((item) => ({ ...item, children: nest(items, item.id) }));
+
+const comments = [
+  { id: 1, parent_id: null },
+  { id: 2, parent_id: 1 },
+  { id: 3, parent_id: 1 },
+  { id: 4, parent_id: 2 },
+  { id: 5, parent_id: 4 },
+];
+const nestedComments = nest(comments);
+console.log(nestedComments);
