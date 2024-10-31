57d685e565deb30b1c2a577c2152aca0dea59d79
Pushkar Pandey <pushkartas2418@gmail.com>
2024-10-31 17:32:44 +0530

    new practice code

---
+++ es6Fundamental/off.js
@@ -0,0 +1,9 @@
+// Define the 'off' function to remove an event listener from an element.
+const off = (el, evt, fn, opts = false) =>
+  el.removeEventListener(evt, fn, opts);
+
+const fn = () => console.log("!");
+
+document.body.addEventListener("click", fn);
+
+console.log(off(document.body, "click", fn));
