af0d4fb54749119045c90008e3fe47809e9d8823
Pushkar Pandey <pushkartas2418@gmail.com>
2024-11-06 19:41:47 +0530

    new practice code

---
+++ es6Fundamental/reducedFilter.js
@@ -0,0 +1,23 @@
+// Define the 'reducedFilter' function to filter an array of objects and return a new array of objects containing only selected keys based on a filtering function.
+const reducedFilter = (data, keys, fn) =>
+  data.filter(fn).map((el) =>
+    keys.reduce((acc, key) => {
+      acc[key] = el[key];
+      return acc;
+    }, {})
+  );
+
+const data = [
+  {
+    id: 1,
+    name: "john",
+    age: 24,
+  },
+  {
+    id: 2,
+    name: "mike",
+    age: 50,
+  },
+];
+
+console.log(reducedFilter(data, ["id", "name"], (item) => item.age > 24));
