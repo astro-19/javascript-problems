dcd206723a327fb386e3d91bcf10f7f9baaa413a
Pushkar Pandey <pushkartas2418@gmail.com>
2024-12-03 21:41:36 +0530

    new practice code

---
+++ es6Fundamental/transform.js
@@ -0,0 +1,14 @@
+// Define the 'transform' function.
+const transform = (obj, fn, acc) =>
+  Object.keys(obj).reduce((a, k) => fn(a, obj[k], k, obj), acc);
+
+console.log(
+  transform(
+    { a: 1, b: 2, c: 1 },
+    (r, v, k) => {
+      (r[v] || (r[v] = [])).push(k);
+      return r;
+    },
+    {}
+  )
+);
