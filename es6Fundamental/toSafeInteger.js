1da0ced2389a4d7fb7a45df212a7591f8836c908
Pushkar Pandey <pushkartas2418@gmail.com>
2024-12-08 00:10:48 +0530

    new practice code

---
+++ es6Fundamental/toSafeInteger.js
@@ -0,0 +1,8 @@
+// Define the 'toSafeInteger' function.
+const toSafeInteger = (num) =>
+  Math.round(
+    Math.max(Math.min(num, Number.MAX_SAFE_INTEGER), Number.MIN_SAFE_INTEGER)
+  );
+
+console.log(toSafeInteger("3.2"));
+console.log(toSafeInteger(Infinity));
