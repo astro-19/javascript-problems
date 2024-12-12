c03382d1b52d530bdd967f78b52b3af71d4336d1
Pushkar Pandey <pushkartas2418@gmail.com>
2024-12-12 20:42:30 +0530

    new practice code

---
+++ es6Fundamental/toDecimalMark.js
@@ -0,0 +1,6 @@
+// Define the 'toDecimalMark' function.
+const toDecimalMark = (num) => num.toLocaleString("en-US");
+
+console.log(toDecimalMark(12305030388.9087));
+console.log(toDecimalMark(123.2264));
+console.log(toDecimalMark(-100.1));
