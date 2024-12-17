376c8e219091fa45046897c22c082a86981d91b7
Pushkar Pandey <pushkartas2418@gmail.com>
2024-12-17 20:45:49 +0530

    new practice code

---
+++ es6Fundamental/takeRightWhile.js
@@ -0,0 +1,10 @@
+// Define the 'takeRightWhile' function.
+const takeRightWhile = (arr, func) => {
+  for (let i of arr.reverse().keys()) {
+    if (!func(arr[i])) return arr.reverse().slice(arr.length - i, arr.length);
+  }
+
+  return arr;
+};
+
+console.log(takeRightWhile([1, 2, 3, 4], (n) => n < 3));
