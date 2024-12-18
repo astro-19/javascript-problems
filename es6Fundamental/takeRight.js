5983422a93c1e729cb7fd053d8406ab50a98cb74
Pushkar Pandey <pushkartas2418@gmail.com>
2024-12-18 20:41:11 +0530

    new practice code

---
+++ es6Fundamental/takeRight.js
@@ -0,0 +1,5 @@
+// Define the 'takeRight' function.
+const takeRight = (arr, n = 1) => arr.slice(arr.length - n, arr.length);
+
+console.log(takeRight([1, 2, 3], 2));
+console.log(takeRight([1, 2, 3]));
