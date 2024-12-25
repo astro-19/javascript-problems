a29f9bd58e871b4b96982730b7feba540772574f
Pushkar Pandey <pushkartas2418@gmail.com>
2024-12-25 20:16:07 +0530

    new practice code

---
+++ es6Fundamental/stableSort.js
@@ -0,0 +1,9 @@
+// Define the 'stableSort' function.
+const stableSort = (arr, compare) =>
+  arr
+    .map((item, index) => ({ item, index }))
+    .sort((a, b) => compare(a.item, b.item) || a.index - b.index)
+    .map(({ item }) => item);
+
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(stableSort(arr, () => 0));
