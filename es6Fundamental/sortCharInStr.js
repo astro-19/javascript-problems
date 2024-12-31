2bcddf65846d709728551f05952dc88c1dc283d0
Pushkar Pandey <pushkartas2418@gmail.com>
2024-12-31 19:28:49 +0530

    new practice code

---
+++ es6Fundamental/sortCharInStr.js
@@ -0,0 +1,6 @@
+// Define the function to sort characters in a string
+const sortCharactersInString = (str) =>
+  [...str].sort((a, b) => a.localeCompare(b)).join("");
+
+console.log(sortCharactersInString("cabbage"));
+console.log(sortCharactersInString("a7fs23l1n4o6x"));
