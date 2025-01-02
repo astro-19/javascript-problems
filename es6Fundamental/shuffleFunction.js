71e5f1322f0791743c4a6a31e1edb5d95161318b
Pushkar Pandey <pushkartas2418@gmail.com>
2025-01-02 21:07:22 +0530

    new practice code

---
+++ es6Fundamental/shuffleFunction.js
@@ -0,0 +1,15 @@
+// Define the shuffle function
+const shuffle = ([...arr]) => {
+  let m = arr.length;
+
+  while (m) {
+    const i = Math.floor(Math.random() * m--);
+
+    [arr[m], arr[i]] = [arr[i], arr[m]];
+  }
+
+  return arr;
+};
+
+const foo = [1, 2, 3];
+console.log(shuffle(foo));
