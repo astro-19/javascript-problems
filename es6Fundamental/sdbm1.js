20f7d21f4af3d8a6291a98b5fc517c72913b3b98
Pushkar Pandey <pushkartas2418@gmail.com>
2025-01-07 21:29:48 +0530

    new practice code

---
+++ es6Fundamental/sdbm1.js
@@ -0,0 +1,15 @@
+// Define the sdbm function
+const sdbm = (str) => {
+  let arr = str.split("");
+  return arr.reduce(
+    (hashCode, currentVal) =>
+      (hashCode =
+        currentVal.charCodeAt(0) +
+        (hashCode << 6) +
+        (hashCode << 16) -
+        hashCode),
+    0
+  );
+};
+
+console.log(sdbm("name"));
