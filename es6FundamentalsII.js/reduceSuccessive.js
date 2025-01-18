b535cd630655d9cec51715763e3ab7d91feb405f
Pushkar Pandey <pushkartas2418@gmail.com>
2025-01-18 21:01:23 +0530

    new practice code

---
+++ es6FundamentalsII.js/reduceSuccessive.js
@@ -0,0 +1,8 @@
+// Define a function 'reduceSuccessive' that applies a binary function to the elements of an array
+// Successively reduce the array using the provided binary function and an initial accumulator value
+const reduceSuccessive = (arr, fn, acc) =>
+  arr.reduce(
+    (res, val, i, arr) => (res.push(fn(res.slice(-1)[0], val, i, arr)), res),
+    [acc]
+  );
+console.log(reduceSuccessive([1, 2, 3, 4, 5, 6], (acc, val) => acc + val, 0));
