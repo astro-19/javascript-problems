efe305ecc955b3db40d9792f5abda52c0e8d3fe5
Pushkar Pandey <pushkartas2418@gmail.com>
2025-01-28 19:56:02 +0530

    new practice code

---
+++ es6FundamentalsII.js/pipeAsyncFunction.js
@@ -0,0 +1,14 @@
+// Define a function 'pipeAsyncFunctions' that takes any number of asynchronous functions as arguments and returns a new asynchronous function
+const pipeAsyncFunctions =
+  (...fns) =>
+  (arg) =>
+    fns.reduce((p, f) => p.then(f), Promise.resolve(arg));
+const sum = pipeAsyncFunctions(
+  (x) => x + 1,
+  (x) => new Promise((resolve) => setTimeout(() => resolve(x + 2), 1000)),
+  (x) => x + 3,
+  async (x) => (await x) + 4
+);
+(async () => {
+  console.log(await sum(5));
+})();
