07dd39f97d681175a1bfc4dfbc8d6bc21d407b8f
Pushkar Pandey <pushkartas2418@gmail.com>
2025-01-29 20:27:12 +0530

    new practice code

---
+++ es6FundamentalsII.js/percentile.js
@@ -0,0 +1,7 @@
+// Define a function 'percentile' that calculates the percentile of a given number in an array
+const percentile = (arr, num) =>
+  (arr.filter((item) => item <= num).length / arr.length) * 100;
+console.log(percentile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5));
+console.log(percentile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10));
+console.log(percentile([1, 2, 3, 4, 5, 6], 3));
+console.log(percentile([1, 2, 3, 4, 5, 6], 4));
