c7b7b2acedad96cd7e237ef15b328971f32bf90e
Pushkar Pandey <pushkartas2418@gmail.com>
2025-02-10 20:51:02 +0530

    new practice code

---
+++ es6FundamentalsII.js/matchesWith.js
@@ -0,0 +1,17 @@
+// Define a function 'matchesWith' that takes three arguments: 'obj', 'source', and 'fn'
+const matchesWith = (obj, source, fn) =>
+  Object.keys(source).every((key) =>
+    obj.hasOwnProperty(key) && fn
+      ? fn(obj[key], source[key], key, obj, source)
+      : obj[key] == source[key]
+  );
+
+const isGreeting = (val) => /^h(?:i|ello)$/.test(val);
+
+console.log(
+  matchesWith(
+    { greeting: "hello" },
+    { greeting: "hi" },
+    (oV, sV) => isGreeting(oV) && isGreeting(sV)
+  )
+);
