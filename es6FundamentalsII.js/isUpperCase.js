2e436dfaed59fca70b1909ca2491cfebd31e31f5
Pushkar Pandey <pushkartas2418@gmail.com>
2025-02-24 21:06:42 +0530

    new practice code

---
+++ es6FundamentalsII.js/isUpperCase.js
@@ -0,0 +1,6 @@
+// Define a function 'isUpperCase' that checks if all characters in a string 'str' are uppercase
+const isUpperCase = (str) => str === str.toUpperCase();
+
+console.log(isUpperCase("ABC"));
+console.log(isUpperCase("A3@$"));
+console.log(isUpperCase("aB4"));
