19d6e1d355163608dfcd0771219ad596f52186a9
Pushkar Pandey <pushkartas2418@gmail.com>
2025-02-25 20:49:49 +0530

    new practice code

---
+++ es6FundamentalsII.js/isUndefined.js
@@ -0,0 +1,3 @@
+// Define a function 'isUndefined' that checks if the value 'val' is strictly equal to undefined
+const isUndefined = (val) => val === undefined;
+console.log(isUndefined(undefined));
