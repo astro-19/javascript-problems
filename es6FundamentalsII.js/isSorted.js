615dd678e1fc5685dfa27eeb2b7d143f9a197c09
Pushkar Pandey <pushkartas2418@gmail.com>
2025-02-26 18:55:26 +0530

    new practice code

---
+++ es6FundamentalsII.js/isSorted.js
@@ -0,0 +1,15 @@
+// Define a function 'isSorted' that checks if the array 'arr' is sorted in either ascending or descending order
+const isSorted = (arr) => {
+  let direction = -(arr[0] - arr[1]);
+
+  for (let [i, val] of arr.entries()) {
+    direction = !direction ? -(arr[i - 1] - arr[i]) : direction;
+
+    if (i === arr.length - 1) return !direction ? 0 : direction;
+    else if ((val - arr[i + 1]) * direction > 0) return 0;
+  }
+};
+
+console.log(isSorted([0, 1, 2, 2]));
+console.log(isSorted([4, 3, 2]));
+console.log(isSorted([4, 3, 5]));
---
+++ es6FundamentalsII.js/isString.js
@@ -0,0 +1,4 @@
+// Define a function 'isString' that checks if the type of value 'val' is 'string'
+const isString = (val) => typeof val === "string";
+
+console.log(isString("10"));
