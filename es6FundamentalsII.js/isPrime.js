66cae42f44c2a463ec81469bae110d3d925efd0d
Pushkar Pandey <pushkartas2418@gmail.com>
2025-03-01 18:37:56 +0530

    new practice code

---
+++ es6FundamentalsII.js/isPrime.js
@@ -0,0 +1,10 @@
+// Define a function 'isPrime' that checks if the given number 'num' is a prime number
+const isPrime = (num) => {
+  const boundary = Math.floor(Math.sqrt(num));
+  for (var i = 2; i <= boundary; i++) if (num % i === 0) return false;
+  return num >= 2;
+};
+
+console.log(isPrime(11));
+console.log(isPrime(17));
+console.log(isPrime(8));
