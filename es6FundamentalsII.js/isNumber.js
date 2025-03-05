e2f32b4f0db72f10f937e1808e3331d77e88c558
Pushkar Pandey <pushkartas2418@gmail.com>
2025-03-05 20:43:01 +0530

    new practice code

---
+++ es6FundamentalsII.js/isNumber.js
@@ -0,0 +1,5 @@
+// Define a function 'isNumber' that checks if the given value 'val' is a number
+const isNumber = (val) => typeof val === "number";
+
+console.log(isNumber("1"));
+console.log(isNumber(1));
