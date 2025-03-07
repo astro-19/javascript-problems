cfcec90f4c1004bdb96a11b342ef4699fee6e280
Pushkar Pandey <pushkartas2418@gmail.com>
2025-03-07 20:46:38 +0530

    new practice code

---
+++ es6FundamentalsII.js/isLowerCase.js
@@ -0,0 +1,6 @@
+// Define a function 'isLowerCase' that checks if the given string 'str' contains only lowercase letters
+const isLowerCase = (str) => str === str.toLowerCase();
+
+console.log(isLowerCase("abc"));
+console.log(isLowerCase("a3@$"));
+console.log(isLowerCase("Ab4"));
