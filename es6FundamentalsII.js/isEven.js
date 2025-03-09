1b33b4758426bdb7cc331a93a550765b1e8f9b20
Pushkar Pandey <pushkartas2418@gmail.com>
2025-03-09 20:15:29 +0530

    new practice code

---
+++ es6FundamentalsII.js/isEven.js
@@ -0,0 +1,7 @@
+// Define a function 'isEven' that checks if the given number 'num' is even
+const isEven = (num) => num % 2 === 0;
+
+console.log(isEven(3));
+console.log(isEven(32));
+console.log(isEven(1));
+console.log(isEven(0));
