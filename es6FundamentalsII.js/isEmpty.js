3180f7e5f14a3436d246b537a8c01c8cbaa5ca38
Pushkar Pandey <pushkartas2418@gmail.com>
2025-03-10 20:48:58 +0530

    new practice code

---
+++ es6FundamentalsII.js/isEmpty.js
@@ -0,0 +1,13 @@
+// Define a function 'isEmpty' that checks if the given value 'val' is empty
+const isEmpty = (val) => val == null || !(Object.keys(val) || val).length;
+
+console.log(isEmpty(new Map()));
+console.log(isEmpty(new Set()));
+console.log(isEmpty([]));
+console.log(isEmpty({}));
+console.log(isEmpty(""));
+console.log(isEmpty([1, 2]));
+console.log(isEmpty({ a: 1, b: 2 }));
+console.log(isEmpty("text"));
+console.log(isEmpty(123));
+console.log(isEmpty(true));
