fc9f058564ab0dabf083e7733634ba8e313cd043
Pushkar Pandey <pushkartas2418@gmail.com>
2025-03-11 20:41:16 +0530

    new practice code

---
+++ es6FundamentalsII.js/isDivisible.js
@@ -0,0 +1,7 @@
+// Define a function 'isDivisible' that checks if 'dividend' is divisible by 'divisor'
+const isDivisible = (dividend, divisor) => dividend % divisor === 0;
+
+console.log(isDivisible(6, 3));
+console.log(isDivisible(5, 3));
+console.log(isDivisible(100, 10));
+console.log(isDivisible(0, 3));
