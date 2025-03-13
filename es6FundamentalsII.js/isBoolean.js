3cfa9bacd5e89e884d779799bd1a243f4753f51c
Pushkar Pandey <pushkartas2418@gmail.com>
2025-03-13 20:49:25 +0530

    new practice code

---
+++ es6FundamentalsII.js/isBoolean.js
@@ -0,0 +1,5 @@
+// Define a function 'isBoolean' that checks if the type of the input value is boolean
+const isBoolean = (val) => typeof val === "boolean";
+
+console.log(isBoolean(null));
+console.log(isBoolean(false));
