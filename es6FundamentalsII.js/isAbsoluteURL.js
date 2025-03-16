0df0225351146c048c85c4d3b9c9aba8e3fed5f2
Pushkar Pandey <pushkartas2418@gmail.com>
2025-03-16 20:42:31 +0530

    new practice code

---
+++ es6FundamentalsII.js/isAbsoluteURL.js
@@ -0,0 +1,6 @@
+// Define a function 'isAbsoluteURL' that checks if the input string is an absolute URL
+const isAbsoluteURL = (str) => /^[a-z][a-z0-9+.-]*:/.test(str);
+
+console.log(isAbsoluteURL("https://google.com"));
+console.log(isAbsoluteURL("ftp://www.myserver.net"));
+console.log(isAbsoluteURL("/foo/bar"));
