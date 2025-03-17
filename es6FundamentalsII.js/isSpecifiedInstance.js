7cd626a1d02e4afdd0f79f70c05d181464bd3568
Pushkar Pandey <pushkartas2418@gmail.com>
2025-03-17 21:12:14 +0530

    new practice code

---
+++ es6FundamentalsII.js/isSpecifiedInstance.js
@@ -0,0 +1,16 @@
+// Define a function 'is' that checks if the provided value is an instance of the specified type
+const is = (type, val) => ![, null].includes(val) && val.constructor === type;
+
+console.log(is(Array, [1]));
+console.log(is(ArrayBuffer, new ArrayBuffer()));
+console.log(is(Map, new Map()));
+console.log(is(RegExp, /./g));
+console.log(is(Set, new Set()));
+console.log(is(WeakMap, new WeakMap()));
+console.log(is(WeakSet, new WeakSet()));
+console.log(is(String, ""));
+console.log(is(String, new String("")));
+console.log(is(Number, 1));
+console.log(is(Number, new Number(1)));
+console.log(is(Boolean, true));
+console.log(is(Boolean, new Boolean(true)));
