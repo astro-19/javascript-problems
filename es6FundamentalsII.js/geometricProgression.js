f6cd91a2987c83558af651c9932d8ffe1b73e540
Pushkar Pandey <pushkartas2418@gmail.com>
2025-03-31 20:16:53 +0530

    new practice code

---
+++ es6FundamentalsII.js/geometricProgression.js
@@ -0,0 +1,9 @@
+// Define a function 'geometricProgression' that generates a geometric progression sequence
+const geometricProgression = (end, start = 1, step = 2) =>
+  Array.from({
+    length: Math.floor(Math.log(end / start) / Math.log(step)) + 1,
+  }).map((v, i) => start * step ** i);
+
+console.log(geometricProgression(256));
+console.log(geometricProgression(256, 3));
+console.log(geometricProgression(256, 1, 4));
