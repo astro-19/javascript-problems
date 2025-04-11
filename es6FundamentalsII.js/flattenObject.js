de10b07a97b18a37dd7ff6a4f3f44e1012a34c35
Pushkar Pandey <pushkartas2418@gmail.com>
2025-04-11 21:09:09 +0530

    new practice code

---
+++ es6FundamentalsII.js/flattenObject.js
@@ -0,0 +1,11 @@
+// Define a function 'flattenObject' that flattens nested objects into a single-level object with dot-separated keys
+const flattenObject = (obj, prefix = "") =>
+  Object.keys(obj).reduce((acc, k) => {
+    const pre = prefix.length ? prefix + "." : "";
+    if (typeof obj[k] === "object")
+      Object.assign(acc, flattenObject(obj[k], pre + k));
+    else acc[pre + k] = obj[k];
+    return acc;
+  }, {});
+
+console.log(flattenObject({ a: { b: { c: 1 } }, d: 1 }));
