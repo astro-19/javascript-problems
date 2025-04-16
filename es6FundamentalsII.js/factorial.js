9231645dced62f73162be90143461c5d97ca4160
Pushkar Pandey <pushkartas2418@gmail.com>
2025-04-16 21:15:22 +0530

    new practice code

---
+++ es6FundamentalsII.js/factorial.js
@@ -0,0 +1,13 @@
+// Define a function 'factorial' to calculate the factorial of a given number 'n'
+const factorial = (n) =>
+  n < 0
+    ? (() => {
+        throw new TypeError("Negative numbers are not allowed!");
+      })()
+    : n <= 1
+    ? 1
+    : n * factorial(n - 1);
+
+console.log(factorial(1));
+console.log(factorial(5));
+console.log(factorial(7));
