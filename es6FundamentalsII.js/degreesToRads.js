f424ebf8285113203bd6cfb9736a4eca517b7ad9
Pushkar Pandey <pushkartas2418@gmail.com>
2025-04-25 21:22:45 +0530

    new practice code

---
+++ es6FundamentalsII.js/degreesToRads.js
@@ -0,0 +1,5 @@
+// Define a function 'degreesToRads' to convert degrees to radians
+const degreesToRads = (deg) => (deg * Math.PI) / 180.0;
+
+console.log(degreesToRads(90.0));
+console.log(degreesToRads(30.0));
