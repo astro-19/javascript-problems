7309bfccd4f533f107bc064a873295d8c91bd3d3
Pushkar Pandey <pushkartas2418@gmail.com>
2025-05-03 19:18:44 +0530

    new practice code

---
+++ es6FundamentalsII.js/RGBToHex.js
@@ -0,0 +1,6 @@
+// Define a function 'RGBToHex' to convert RGB values to hexadecimal format
+const RGBToHex = (r, g, b) =>
+  ((r << 16) + (g << 8) + b).toString(16).padStart(6, "0");
+
+console.log(RGBToHex(255, 165, 1));
+console.log(RGBToHex(255, 255, 1));
