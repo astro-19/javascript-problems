f1c34f3ebe7438a7f3c06c97b7937d27b9630dae
Pushkar Pandey <pushkartas2418@gmail.com>
2025-05-17 20:52:50 +0530

    new practice code

---
+++ es6FundamentalsII.js/chunk.js
@@ -0,0 +1,7 @@
+// Define a function 'chunk' to split an array into smaller chunks of a specified size
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+    arr.slice(i * size, i * size + size)
+  );
+
+console.log(chunk([1, 2, 3, 4, 5], 2));
