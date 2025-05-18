5ee46893b867e55530e471360c026cdbcab6e10c
Pushkar Pandey <pushkartas2418@gmail.com>
2025-05-18 20:46:55 +0530

    new practice code

---
+++ es6FundamentalsII.js/clampNumber.js
@@ -0,0 +1,6 @@
+// Define a function 'clampNumber' to restrict a number within a specified range
+const clampNumber = (num, a, b) =>
+  Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b));
+
+console.log(clampNumber(2, 3, 5));
+console.log(clampNumber(1, -1, -5));
