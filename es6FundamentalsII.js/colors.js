7fc9a54fae078e70d5302715a2cef76a30f19502
Pushkar Pandey <pushkartas2418@gmail.com>
2025-05-20 20:09:26 +0530

    new practice code

---
+++ es6FundamentalsII.js/colors.js
@@ -0,0 +1,6 @@
+// Define an array 'colors' containing color names
+const colors = ["Red", "Green", "Black", "White"];
+
+for (let [index, item] of colors.entries()) {
+  console.log(`${index}: ${item}`);
+}
