665ed7f151c39fd4fe8be70e7bf3d9acb0fffad7
Pushkar Pandey <pushkartas2418@gmail.com>
2025-05-21 20:50:55 +0530

    new practice code

---
+++ recursion/factorial.js
@@ -0,0 +1,10 @@
+// Recursive JavaScript function to calculate the factorial of a number.
+function factorial(x) {
+  if (x === 0) {
+    return 1;
+  }
+
+  return x * factorial(x - 1);
+}
+
+console.log(factorial(5));
