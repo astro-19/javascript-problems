e22c6a415f77a79c8fca2b668283ab9161a152cd
Pushkar Pandey <pushkartas2418@gmail.com>
2025-05-23 19:29:33 +0530

    new practice code

---
+++ recursion/range.js
@@ -0,0 +1,12 @@
+// Function to generate a range of numbers between start_num and end_num (exclusive).
+var range = function (start_num, end_num) {
+  if (end_num - start_num === 2) {
+    return [start_num + 1];
+  } else {
+    var list = range(start_num, end_num - 1);
+    list.push(end_num - 1);
+    return list;
+  }
+};
+
+console.log(range(2, 9));
