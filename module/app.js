c98239b4a650b6cd972d838db1c8043c14aff9a9
Pushkar Pandey <pushkartas2418@gmail.com>
2026-01-09 10:06:10 +0530

    new practice code

--- module/app.js
+++ module/app.js
@@ -1,6 +1,14 @@
-// --- file: app.js ---
 // Import the default function
 import greet from './greet.js';
+// Import specific functions
+import { add, subtract, multiply, divide } from './arithmetic.js';
 
 // Use the imported function
-console.log(greet('Sara')); 
+console.log(greet('Sara'));
+
+// Use the imported functions
+console.log(add(10, 5));
+console.log(subtract(10, 5));
+console.log(multiply(10, 5));
+console.log(divide(10, 5));
+
