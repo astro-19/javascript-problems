e46dcf4dbca6f8e24141fcc4a635e89cf8a0722b
Pushkar Pandey <pushkartas2418@gmail.com>
2026-02-04 11:00:13 +0530

    new practice code

---
+++ module/commonJsModule.js
@@ -0,0 +1,6 @@
+// Exporting a function using CommonJS
+module.exports = {
+    greet: function (name) {
+        return `Hello, ${name}!`;
+    },
+};
---
+++ module/es6Module.js
@@ -0,0 +1,5 @@
+// Importing the CommonJS module in an ES6 module
+import commonJSModule from './commonJsModule.js';
+
+// Using the imported module
+console.log(commonJSModule.greet('Sara')); // Logs "Hello, Alice!"
